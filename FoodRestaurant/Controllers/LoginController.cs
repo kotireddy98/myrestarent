52a4d2121eb6c5d5ba34ba227208f42f61a52feb
koti <s.kotireddy1998@gmail.com>
2024-11-22 08:27:01 +0530

    this my change

---
+++ FoodRestaurant/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using FoodRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodRestaurant.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public class Users
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        private static readonly List<Users> list = new List<Users>
+        {
+            new Users { Username = "Admin", Password = "12345" },
+            new Users { Username = "Gopi", Password = "12345" },
+            new Users { Username = "Kota", Password = "12345" }
+        };
+
+
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var userd = list.Find(u => u.Username == username && u.Password == password);
+
+            var user = list.Find(u => string.Equals(u.Username, username, StringComparison.OrdinalIgnoreCase)
+                                                 && u.Password == password);
+
+            if (user != null)
+            {
+                Session["LoggedInUser"] = user.Username;
+
+                return RedirectToAction("Home", "Home");
+            }
+            ViewBag.ErrorMessage = "Invalid username or password.";
+            return View();
+        }
+    }
+}
---
+++ FoodRestaurant/Controllers/OrderController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodRestaurant.Controllers
+{
+    public class OrderController : Controller
+    {
+        private static List<OrderBO> Orders = new List<OrderBO>
+    {
+        new OrderBO { OrderID = 1, OrderName = "Order1", Price = 100, Quantity = 2, Addresses = "Home Address" },
+        new OrderBO { OrderID = 2, OrderName = "Order2", Price = 200, Quantity = 1, Addresses = "Office Address" }
+    };
+        public class OrderBO
+        {
+            public int OrderID { get; set; }         
+            public string OrderName { get; set; }   
+            public decimal Price { get; set; }     
+            public int? Quantity { get; set; }       
+            public string Addresses { get; set; }   
+        }
+
+
+        // GET: Order
+        public ActionResult Order()
+        {
+            return View(Orders);
+        }
+
+        public ActionResult CreateOrder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateOrder(OrderBO orderBO)
+        {
+            // Generate a new OrderID
+            orderBO.OrderID = Orders.Max(o => o.OrderID) + 1;
+            Orders.Add(orderBO);
+            return RedirectToAction("Order");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var order = Orders.FirstOrDefault(o => o.OrderID == id);
+            return View(order);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(OrderBO orderBO)
+        {
+            var existingOrder = Orders.FirstOrDefault(o => o.OrderID == orderBO.OrderID);
+            if (existingOrder != null)
+            {
+                existingOrder.OrderName = orderBO.OrderName;
+                existingOrder.Price = orderBO.Price;
+                existingOrder.Quantity = orderBO.Quantity;
+                existingOrder.Addresses = orderBO.Addresses;
+            }
+            return RedirectToAction("Order");
+        }
+
+        public ActionResult DeleteOrder(int id)
+        {
+            var order = Orders.FirstOrDefault(o => o.OrderID == id);
+            if (order != null)
+            {
+                Orders.Remove(order);
+            }
+            return RedirectToAction("Order");
+        }
+    }
+}
